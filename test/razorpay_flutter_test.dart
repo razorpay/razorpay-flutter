b38a9e44546ab16d8e1bfa1ca8751ba0ccbc2390
Chintan Acharya <chintan.acharya@razorpay.com>
2019-05-03 16:24:06 +0530

    Add sanity tests

--- test/razorpay_flutter_test.dart
+++ test/razorpay_flutter_test.dart
@@ -3,15 +3,72 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('razorpay_flutter');
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+  group("$Razorpay", () {
+    const MethodChannel channel = MethodChannel("razorpay_flutter");
+
+    final List<MethodCall> log = <MethodCall>[];
+
+    Razorpay razorpay;
+
+    setUp(() {
+      channel.setMockMethodCallHandler((MethodCall call) async {
+        log.add(call);
+        return {};
+      });
+
+      razorpay = Razorpay();
+
+      log.clear();
     });
-  });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    group("#open", () {
+
+      setUp(() {
+        razorpay.clear();
+      });
+
+      test('passes options correctly', () async {
+        var options = {
+          'key': 'rzp_test_1DP5mmOlF5G5aa',
+          'amount': 2000,
+          'name': 'Acme Corp.',
+          'description': 'Fine T-Shirt',
+          'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
+          'external': {
+            'wallets': ['paytm']
+          }
+        };
+
+        razorpay.open(options);
+
+        expect(log, <Matcher>[
+          isMethodCall('open', arguments: options)
+        ]);
+      });
+
+      test('throws error if key is not passed', () async {
+
+        var options = {
+          'amount': 2000,
+          'name': 'Acme Corp.',
+          'description': 'Fine T-Shirt',
+          'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
+          'external': {
+            'wallets': ['paytm']
+          }
+        };
+
+        var errorHandler = (PaymentFailureResponse response) {
+          expect(response.code, equals(Razorpay.INVALID_OPTIONS));
+        };
+
+        razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, expectAsync1(errorHandler, count: 1));
+
+        razorpay.open(options);
+
+      });
+
+    });
   });
 }
