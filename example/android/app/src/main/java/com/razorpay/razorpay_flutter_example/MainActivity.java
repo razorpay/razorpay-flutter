e09d3b7c0f80c36195fb59719c5fd0ebed8c3e01
sumedhtambat <sumedh.tambat@gmail.com>
2020-05-20 17:11:33 +0530

    - Updated to version 1.2.1

--- example/android/app/src/main/java/com/razorpay/razorpay_flutter_example/MainActivity.java
+++ example/android/app/src/main/java/com/razorpay/razorpay_flutter_example/MainActivity.java
@@ -1,13 +1,23 @@
 package com.razorpay.razorpay_flutter_example;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+
+import androidx.annotation.NonNull;
+
+import com.razorpay.razorpay_flutter.RazorpayDelegate;
+import com.razorpay.razorpay_flutter.RazorpayFlutterPlugin;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+  private ActivityPluginBinding pluginBinding;
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new RazorpayFlutterPlugin());
   }
 }
