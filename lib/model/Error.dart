c27bf5d0236f763efdc2074a2e0400239b766a04
Shibu <shibu.prasad@razorpay.com>
2023-12-26 16:18:34 +0530

    Update Error model

--- lib/model/Error.dart
+++ lib/model/Error.dart
@@ -1,19 +1,28 @@
 class Error {
   String? errorCode;
   String? errorDescription;
-  Error({ this.errorCode,  this.errorDescription});
+  String? errorReason;
+  String? errorStep;
+  String? errorSource;
+  Error({ this.errorCode,  this.errorDescription, this.errorReason, this.errorStep, this.errorSource});
 
   factory Error.fromJson(Map<String, dynamic> json) {
     return Error(
       errorCode: json['errorCode'] ,
-      errorDescription: json['errorDescription']
+      errorDescription: json['errorDescription'] ,
+      errorReason: json['errorReason'] ,
+      errorStep: json['errorSource'] ,
+      errorSource: json['errorStep']
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'errorCode': errorCode,
-      'errorDescription': errorDescription
+      'errorDescription': errorDescription,
+      'errorReason': errorReason,
+      'errorSource': errorSource,
+      'errorStep': errorStep
     };
   }
 
--- lib/upi_turbo.dart
+++ lib/upi_turbo.dart
@@ -39,11 +39,11 @@ class UpiTurbo {
       if(getLinkedUpiAccountsResponse["data"]!=""){
         onSuccess(_getUpiAccounts(getLinkedUpiAccountsResponse["data"]));
       }else {
-        onFailure(Error(errorCode:"" , errorDescription: "No Account Found"));
+        onFailure(Error(errorCode:"" , errorDescription: "No Account Found", errorReason: "sdk_error", errorSource: "unknown_source", errorStep: ""));
       }
 
     } on PlatformException catch (error) {
-      onFailure(Error(errorCode:error.code , errorDescription: error.message!));
+      onFailure(Error(errorCode:error.code , errorDescription: error.message!, errorReason: "sdk_error", errorSource: "unknown_source", errorStep: ""));
     }
   }
 
@@ -61,7 +61,7 @@ class UpiTurbo {
 
       await _channel.invokeMethod('manageUpiAccounts', requestManageUpiAccounts);
     } on PlatformException catch (error) {
-      onFailure(Error(errorCode:error.code , errorDescription: error.message!));
+      onFailure(Error(errorCode:error.code , errorDescription: error.message!, errorReason: "sdk_error", errorSource: "unknown_source", errorStep: ""));
     }
   }
 
@@ -77,7 +77,7 @@ class UpiTurbo {
   }
 
   void _emitFailure(OnFailure<Error> onFailure) {
-    onFailure(Error(errorCode:"" , errorDescription: "No Turbo Plugin Found"));
+    onFailure(Error(errorCode:"" , errorDescription: "No Turbo Plugin Found", errorReason: "sdk_error", errorSource: "unknown_source", errorStep: ""));
   }
 
 }
